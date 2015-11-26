7b3c8953a3e16e00bebb419966f824a90e921012
Vladimir <Vladimir.Ivantchenko@cern.ch>
2015-11-26 09:48:35 +0100

    removed commented part of code

--- include/MyDetectorConstruction.hh
+++ include/MyDetectorConstruction.hh
@@ -2,18 +2,12 @@
 #define MyDetectorConstruction_H 1
 
 #include "G4VUserDetectorConstruction.hh"
-
 #include "G4GDMLParser.hh"
-
-//#include "G4Cache.hh"
-//#include "G4Threading.hh"
 
 class G4VPhysicalVolume;
 class G4FieldManager;
 class G4UniformMagField;
-//class G4Material;
 class MyDetectorMessenger;
-
 
 class MyDetectorConstruction : public G4VUserDetectorConstruction {
 
@@ -26,22 +20,7 @@ public:
 
   void ConstructSDandField();
 
-  //01.25.2009 Xin Dong: Used by worker threads to achieve the partial
-  //effect similar to the member function Construct() invoked by the
-  //master thread.
-  //G4VPhysicalVolume* ConstructSlave();
-
   void SetMagField( const G4double fieldValue );
-
-  //01.25.2009 Xin Dong: Used by worker threads to achieve the partial
-  //effect similar to the constructor implicitly invoked by the master
-  //thread.
-  //void SlaveMyDetectorConstruction();
-
-  //01.25.2009 Xin Dong: Use by worker threads to achieve the partial
-  //effect similar to the destructor invoked by the master thread.
-  //void SlaveDestroy();
-
   
 private:
   
